92493737deaad5e9c7799e5587fdac9966b80656
shivam-jainn <shivam.clgstash@gmail.com>
2024-12-14 00:27:52 +0530

    feat(ws): :art: added error handling for ws url handling in .envs

--- src/lib/websockets/ws.tsx
+++ src/lib/websockets/ws.tsx
@@ -1,8 +1,20 @@
 import useWebSocket from 'react-use-websocket';
 
-const socketUrl = 'wss://echo.websocket.org';
+const socketUrl = process.env.BINANCE_WSS;
+
 
 export const wsClient = () => {
+  if(!socketUrl){
+    return {
+      sendMessage: () => {},
+      sendJsonMessage: () => {},
+      lastMessage: null,
+      lastJsonMessage: null,
+      readyState: -1,
+      getWebSocket: () => null,
+    };
+  }
+
   const socket = useWebSocket(socketUrl, {
    onOpen: () => console.log('opened'),
    shouldReconnect: (closeEvent) => true,
